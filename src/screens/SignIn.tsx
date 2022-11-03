bb22322adefa87c19d5be59e67b4b12322d86113
Felipe Lustosa <felipelustosaa6@gmail.com>
2022-11-02 21:03:18 -0300

    global changes

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,14 @@
-import { Text, Center } from "native-base";
+import { Text, Center, Icon } from "native-base";
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
+import { Fontisto } from "@expo/vector-icons";
 
 export function SignIn() {
     return (
         <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24} fontFamily="heading">
-                Sign in
-            </Text>
+            <Logo width={212} height={40} />
+
+            <Button title="ENTRAR COM GOOGLE" type="SECONDARY" leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} />
         </Center>
     );
 }
