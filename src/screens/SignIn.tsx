34020ac2bf10c207925fe437304f840d4bfac3f6
Felipe Lustosa <felipelustosaa6@gmail.com>
2022-11-02 21:07:13 -0300

    sign screen front

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -5,10 +5,14 @@ import { Fontisto } from "@expo/vector-icons";
 
 export function SignIn() {
     return (
-        <Center flex={1} bgColor="gray.900">
+        <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
 
-            <Button title="ENTRAR COM GOOGLE" type="SECONDARY" leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} />
+            <Button title="ENTRAR COM GOOGLE" type="SECONDARY" leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} mt={12} />
+
+            <Text color="white" textAlign="center" mt={4}>
+                Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação de sua conta.
+            </Text>
         </Center>
     );
 }
