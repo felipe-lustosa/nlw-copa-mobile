5281ad0f98622e7d2f4240860fe692ad683abfc5
Felipe Lustosa <felipelustosaa6@gmail.com>
2022-11-03 20:35:24 -0300

    create, find and my pools front end

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
 import { SignIn } from "./src/screens/SignIn";
 import { AuthContextProvider } from "./src/context/authContext";
+import { CreatePool } from "./src/screens/CreatePool";
+import { FindPool } from "./src/screens/FindPool";
+import { MyPools } from "./src/screens/MyPools";
 
 export default function App() {
     const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -12,7 +15,7 @@ export default function App() {
         <NativeBaseProvider theme={THEME}>
             <AuthContextProvider>
                 <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-                {fontsLoaded ? <SignIn /> : <Loading />}
+                {fontsLoaded ? <MyPools /> : <Loading />}
             </AuthContextProvider>
         </NativeBaseProvider>
     );
